00e386cfcba5f7803f078d15691af00d2b753fbe
Leonardo <https://github.com/Leonardo-tech-stack/sound-garden-test>
2023-03-03 12:54:07 -0300

    index pronto

--- Js/index.js
+++ Js/index.js
@@ -9,56 +9,82 @@ const nav = document.querySelector("nav");
 nav.children[0].href = "/cursos.html";
 
 // No banner da home, está faltando centralizar os elementos filhos da section que tem o id #introdução.
-const divIntrodução = document.querySelector("#introducao");
+const divIntroducao = document.querySelector("#introducao");
 
 divIntroducao.style = "justfy-content: space-evenly";
 
-// Na etapa de depoimentos, o título deveria ser "O que falam de nós"
+// Na etapa de depoimentos, o título deveria ser "O que falam sobre nós"
 const tituloDepoimentos = document.querySelector(".titulo");
 
-tituloDepoimentos.innerHTML = "<h3>O que falam de nós</h3>";
+tituloDepoimentos.innerHTML = "<h3>O que falam sobre nós</h3>";
 
 // Na etapa de blog, o título deveria ser "Mais conteúdo pra você"
-const tituloDepoimentos = document.querySelector(".titulo");
+const tituloBlog = document.querySelectorAll(".titulo");
 
 tituloBlog[1].innerHTML = "<h3>Mais conteúdo pra você</h3>";
 
 // Todos os textos que estiverem com a classe .titulo devem apresentar todas as letras maiúsculas
-document
-.querySelectorAll(".titulo")
-.forEach((titulo) => (titulo.stylr = "text-transform: uppercase;"));
+document.querySelectorAll(".titulo")
+.forEach((titulo) => (titulo.style = "text-transform: uppercase;"));
 
 // O  botão "ver todos os posts" deve ter um link que direciona para o arquivo blog.html
 const aPosts = document.querySelector("#todos_posts");
 
-aPosts[].href = "/blog.html";                                                                                       //adicionar index
+aPosts.href = "/blog.html";                                                                                       
 
 // Adicionar um novo curso na section que contém o id investimentos_poupando_independencia
-const sectionInvestimentos = document.querySelector("#investimentos_poupando_independencia");
+const investimentos_poupando_independencia = document.getElementById("investimentos_poupando_independencia");
 
-sectionInvestimentos.innerHTML = '<div id="independencia"><img src="/imagens/independencia_financeira.png" width="180px"alt="Independência Financeira"><h2>Independência Financeira</h2><p>Duis aute irure dolor in reprehenderit in voluptate velit essecillum doloreeu fugiat nulla pariatur. </p><a class="comecar_agora" href="./curso.html">começar agora</a></div>'
+investimentos_poupando_independencia.insertAdjacentHTML("beforeend", `
+    <div id="independencia">
+        <img src="/imagens/independencia_financeira.png" width="180px" alt="Independência Financeira">
+        <h2>Independência Financeira</h2>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+        <a class="comecar_agora" href="./curso.html">começar agora</a>
+    </div>
+`);
 
 // O formulário não está funcionando, o atributo action deve mandar para url sucesso.html
-const form = document.querySelector("form");
+const form = document.querySelector('form');
 
-form.children[0].href = "/sucesso.html";
+form.addEventListener('submit', (event) => {   // adiciona o evento de submit no formulário
+    event.preventDefault();     // impede que a página seja recarregada
+    form.setAttribute('action', 'sucesso.html');     // altera o atributo "action" do formulário para redirecionar para "sucesso.html"
+    form.submit();     // envia o formulário
+});
 
 // Após o campo de email, precisamos de um novo campo para que o usuário adicione também um número de telefone
-const formInput = document.querySelector(".email");
-
-formInput.innerHTML = '<input type="telefone" required placeholder="telefone">';
+const emailInput = document.querySelector('input[type="email"]');
+const phoneInput = document.createElement('input');
+phoneInput.setAttribute('type', 'tel');
+phoneInput.setAttribute('name', 'phone');
+phoneInput.setAttribute('placeholder', 'Telefone');
+emailInput.after(phoneInput);
 
 // O campo de mensagem está ultrapassando o tamanho do elemento pai. Podemos resolver adicionando o atributo box-sizing: border-box
-const formTextarea = document.querySelector(".textarea");
+const textarea = document.querySelector('#mensagem');
 
-formTextarea.style = "box-sizing: border-box";
+textarea.style.boxSizing = 'border-box';
 
 // O botão não está do tamanho adequado, precisa ter uma largura maior
-const enviar = document.querySelector("#enviar");
+let btnEnviar = document.getElementById("enviar");
 
-enviar.style = "padding: 100px;"
+btnEnviar.style.width = "120px";
 
 // Abaixo da section do formulário, adicione uma seção de comentário igual a página Home
-const sectionForm = document.querySelector(".formulario");
+const formSection = document.querySelector('.formulario');
 
-formInput.innerHTML = <section class='titulo depoimento'><h3>Depoimentos</h3></section><section id='falam_sobre'><div class="depoimentos"><img src="/imagens/icon-wally.png" width="80px" height="80px" alt="depoimentos da dindim"> <p> ”Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.”<br><br> Wally, 25</p></div><div class="depoimentos"><img src="/imagens/icon-jaden.png" width="80px" height="80px" alt="depoimentos da dindim"> <p> ”Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.” <br> <br> Jaden Smith, 23</p></div><div class="depoimentos"> <img src="/imagens/icon-whoopi.png" width="80px" height="80px" alt="depoimentos da dindim">  <p>  ”Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.” <br> <br> Whoopi Goldberg, 37 </p></div><div class="depoimentos"> <img src="/imagens/icon-jane.png" width="80px" height="80px" alt="depoimentos da dindim"> <p>”Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam.” <br><br>Janes Joplin, 29</p></div></section>;
+const comentarioSection = `<section id="comentario">
+                            <h2>Comentários</h2>
+                            <div class="comentario">
+                                <img src="imagens/avatar-mulher.jpg" alt="avatar mulher">
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sagittis, quam ac auctor dignissim, sapien enim euismod nulla, eget ullamcorper elit dolor ut augue."</p>
+                                <p class="nome">Larissa Santos, 28</p>
+                            </div>
+                            <div class="comentario">
+                                <img src="imagens/avatar-homem.jpg" alt="avatar homem">
+                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sagittis, quam ac auctor dignissim, sapien enim euismod nulla, eget ullamcorper elit dolor ut augue."</p>
+                                <p class="nome">Lucas Andrade, 31</p>
+                            </div>
+                          </section>`;
+formSection.insertAdjacentHTML('afterend', comentarioSection);
